31bcbaf50f18377dc80880f7caba8f0b5b4b4a4a
Tim Murphy <tim@26tp.com>
2013-07-05 11:54:01 +1000

    Changed project name from Elmah.RavenDB to ElmahMagic.RavenDB to prevent confusion with original project.

--- CodeBase/Elmah.RavenDB/Properties/AssemblyInfo.cs
+++ CodeBase/Elmah.RavenDB/Properties/AssemblyInfo.cs
@@ -22,15 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3e0daf6c-1ad5-4a4a-bb14-45df30ae8be8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+// Assembly version is stamped by MyGet build services.
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
